1600bbf123ec9879af9bb96c533d162391026871
DESKTOP-81380DR\Hyeon2-400 <choihg1014@gmail.com>
2024-08-29 18:20:49 +0900

    [#main] UI 조정 및 씬마다 이쁘게 배치

--- Assets/3-Scripts/Monster/Sloth/Turret.cs
+++ Assets/3-Scripts/Monster/Sloth/Turret.cs
@@ -5,244 +5,246 @@ using Spine;
 
 public class Turret : Monster
 {
-    public GameObject bulletPrefab;
-    [SerializeField]
-    private TurretMonsterData stat;
+	public GameObject bulletPrefab;
+	[SerializeField]
+	private TurretMonsterData stat;
 
-    [SpineAnimation] public string summonAnimation;
-    [SpineAnimation] public string idleAnimation;
-    [SpineAnimation] public string frontAttackAnimation;
-    [SpineAnimation] public string backAttackAnimation;
-    [SpineAnimation] public string sideAttackAnimation;
-    [SpineAnimation] public string reloadAnimation;
-    [SpineAnimation] public string reloadIdleAnimation;
+	[SpineAnimation] public string summonAnimation;
+	[SpineAnimation] public string idleAnimation;
+	[SpineAnimation] public string frontAttackAnimation;
+	[SpineAnimation] public string backAttackAnimation;
+	[SpineAnimation] public string sideAttackAnimation;
+	[SpineAnimation] public string reloadAnimation;
+	[SpineAnimation] public string reloadIdleAnimation;
 
-    private SkeletonAnimation skeletonAnimation;
+	private SkeletonAnimation skeletonAnimation;
 
-    [SerializeField] private Transform[] firePoints;
+	[SerializeField] private Transform[] firePoints;
+	[SerializeField] private int bulietQuantity = 2;
+
     private int currentFirePoint = 0;
-    private int attackCount = 0;
-    public bool isAttacking = false;
+	private int attackCount = 0;
+	public bool isAttacking = false;
 
-    protected override void Start()
-    {
-        base.Start();
-        skeletonAnimation = GetComponent<SkeletonAnimation>();
-        if (skeletonAnimation == null)
-        {
-            Debug.LogError("스켈레톤 애니메이션 is null");
-        }
-        else
-        {
-            StartCoroutine(SummonAnimationCoroutine());
-        }
+	protected override void Start()
+	{
+		base.Start();
+		skeletonAnimation = GetComponent<SkeletonAnimation>();
+		if (skeletonAnimation == null)
+		{
+			Debug.LogError("스켈레톤 애니메이션 is null");
+		}
+		else
+		{
+			StartCoroutine(SummonAnimationCoroutine());
+		}
 
-        if (firePoints == null || firePoints.Length < 2)
-        {
-            Debug.LogError("Fire Points가 설정되지 않았거나 부족합니다.");
-        }
-    }
+		if (firePoints == null || firePoints.Length < 2)
+		{
+			Debug.LogError("Fire Points가 설정되지 않았거나 부족합니다.");
+		}
+	}
 
-    private IEnumerator SummonAnimationCoroutine()
-    {
-        PlayAnimation(summonAnimation, false);
-        yield return new WaitForSpineAnimationComplete(skeletonAnimation);
-        TransitionToState(idleState);
-    }
+	private IEnumerator SummonAnimationCoroutine()
+	{
+		PlayAnimation(summonAnimation, false);
+		yield return new WaitForSpineAnimationComplete(skeletonAnimation);
+		TransitionToState(idleState);
+	}
 
-    protected override void InitializeStates()
-    {
-        idleState = new TurretIdleState(this);
-        chaseState = new TurretChaseState(this);
-        attackState = new TurretAttackState(this);
-        cooldownState = new TurretCooldownState(this);
-        currentState = idleState;
-    }
+	protected override void InitializeStates()
+	{
+		idleState = new TurretIdleState(this);
+		chaseState = new TurretChaseState(this);
+		attackState = new TurretAttackState(this);
+		cooldownState = new TurretCooldownState(this);
+		currentState = idleState;
+	}
 
-    public override void Attack()
-    {
-        if (!isAttacking)
-        {
-            StartCoroutine(AttackCoroutine());
-        }
-    }
+	public override void Attack()
+	{
+		if (!isAttacking)
+		{
+			StartCoroutine(AttackCoroutine());
+		}
+	}
 
-    private IEnumerator AttackCoroutine()
-    {
-        isAttacking = true;
-        UpdateSkinAndAnimation();
-        FireBullet();
-        attackCount++;
-        yield return new WaitForSpineAnimationComplete(skeletonAnimation);
+	private IEnumerator AttackCoroutine()
+	{
+		isAttacking = true;
+		UpdateSkinAndAnimation();
+		FireBullet();
+		attackCount++;
+		yield return new WaitForSpineAnimationComplete(skeletonAnimation);
 
-        if (attackCount >= 4)
-        {
-            attackCount = 0;
-            PlayAnimation(reloadAnimation, false);
-            yield return new WaitForSpineAnimationComplete(skeletonAnimation);
-            PlayAnimation(reloadIdleAnimation, true);
-            isAttacking = false;
-            TransitionToState(cooldownState);
-        }
-        else
-        {
-            isAttacking = false;
-            TransitionToState(idleState);
-        }
-    }
+		if (attackCount >= bulietQuantity)
+		{
+			attackCount = 0;
+			PlayAnimation(reloadAnimation, false);
+			yield return new WaitForSpineAnimationComplete(skeletonAnimation);
+			PlayAnimation(reloadIdleAnimation, true);
+			isAttacking = false;
+			TransitionToState(cooldownState);
+		}
+		else
+		{
+			isAttacking = false;
+			TransitionToState(idleState);
+		}
+	}
 
-    private void UpdateSkinAndAnimation()
-    {
-        Vector3 directionToPlayer = player.transform.position - transform.position;
-        string skinName;
-        string attackAnimationName;
+	private void UpdateSkinAndAnimation()
+	{
+		Vector3 directionToPlayer = player.transform.position - transform.position;
+		string skinName;
+		string attackAnimationName;
 
-        if (Mathf.Abs(directionToPlayer.y) > Mathf.Abs(directionToPlayer.x))
-        {
-            if (directionToPlayer.y > 0)
-            {
-                skinName = "back";
-                attackAnimationName = backAttackAnimation;
-            }
-            else
-            {
-                skinName = "front";
-                attackAnimationName = frontAttackAnimation;
-            }
-        }
-        else
-        {
-            skinName = "side";
-            attackAnimationName = sideAttackAnimation;
+		if (Mathf.Abs(directionToPlayer.y) > Mathf.Abs(directionToPlayer.x))
+		{
+			if (directionToPlayer.y > 0)
+			{
+				skinName = "back";
+				attackAnimationName = backAttackAnimation;
+			}
+			else
+			{
+				skinName = "front";
+				attackAnimationName = frontAttackAnimation;
+			}
+		}
+		else
+		{
+			skinName = "side";
+			attackAnimationName = sideAttackAnimation;
 
-            if (directionToPlayer.x > 0)
-            {
-                transform.localScale = new Vector3(-0.3f, 0.3f, 1);
-            }
-            else
-            {
-                transform.localScale = new Vector3(0.3f, 0.3f, 1);
-            }
-        }
+			if (directionToPlayer.x > 0)
+			{
+				transform.localScale = new Vector3(-0.3f, 0.3f, 1);
+			}
+			else
+			{
+				transform.localScale = new Vector3(0.3f, 0.3f, 1);
+			}
+		}
 
-        skeletonAnimation.Skeleton.SetSkin(skinName);
-        PlayAnimation(attackAnimationName, false);
-    }
+		skeletonAnimation.Skeleton.SetSkin(skinName);
+		PlayAnimation(attackAnimationName, false);
+	}
 
-    private void FireBullet()
-    {
-        Vector3 direction = (player.transform.position - firePoints[currentFirePoint].position).normalized;
-        GameObject bullet = Instantiate(bulletPrefab, firePoints[currentFirePoint].position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody2D>().velocity = direction * stat.attackSpeed;
+	private void FireBullet()
+	{
+		Vector3 direction = (player.transform.position - firePoints[currentFirePoint].position).normalized;
+		GameObject bullet = Instantiate(bulletPrefab, firePoints[currentFirePoint].position, Quaternion.identity);
+		bullet.GetComponent<Rigidbody2D>().velocity = direction * stat.attackSpeed;
 
-        Bullet bulletScript = bullet.GetComponent<Bullet>();
-        if (bulletScript != null)
-        {
-            bulletScript.SetAttackDamage(monsterBaseStat.attackDamage);
-        }
+		Bullet bulletScript = bullet.GetComponent<Bullet>();
+		if (bulletScript != null)
+		{
+			bulletScript.SetAttackDamage(monsterBaseStat.attackDamage);
+		}
 
-        currentFirePoint = (currentFirePoint + 1) % firePoints.Length;
-    }
+		currentFirePoint = (currentFirePoint + 1) % firePoints.Length;
+	}
 
-    public void PlayAnimation(string animationName, bool loop)
-    {
-        if (skeletonAnimation != null && !string.IsNullOrEmpty(animationName))
-        {
-            skeletonAnimation.state.SetAnimation(0, animationName, loop);
-        }
-    }
+	public void PlayAnimation(string animationName, bool loop)
+	{
+		if (skeletonAnimation != null && !string.IsNullOrEmpty(animationName))
+		{
+			skeletonAnimation.state.SetAnimation(0, animationName, loop);
+		}
+	}
 }
 
 public class TurretIdleState : MonsterState
 {
-    public TurretIdleState(Monster monster) : base(monster) { }
+	public TurretIdleState(Monster monster) : base(monster) { }
 
-    public override void EnterState()
-    {
-        (monster as Turret)?.PlayAnimation((monster as Turret).idleAnimation, true);
-    }
+	public override void EnterState()
+	{
+		(monster as Turret)?.PlayAnimation((monster as Turret).idleAnimation, true);
+	}
 
-    public override void UpdateState()
-    {
-        if (!(monster as Turret).isAttacking && monster.IsPlayerInRange(monster.monsterBaseStat.detectionRange))
-        {
-            monster.TransitionToState(monster.chaseState);
-        }
-    }
+	public override void UpdateState()
+	{
+		if (!(monster as Turret).isAttacking && monster.IsPlayerInRange(monster.monsterBaseStat.detectionRange))
+		{
+			monster.TransitionToState(monster.chaseState);
+		}
+	}
 
-    public override void ExitState() { }
+	public override void ExitState() { }
 }
 
 public class TurretChaseState : MonsterState
 {
-    public TurretChaseState(Monster monster) : base(monster) { }
+	public TurretChaseState(Monster monster) : base(monster) { }
 
-    public override void EnterState() { }
+	public override void EnterState() { }
 
-    public override void UpdateState()
-    {
-        if (!(monster as Turret).isAttacking && monster.IsPlayerInRange(monster.monsterBaseStat.attackRange))
-        {
-            monster.TransitionToState(monster.attackState);
-        }
-        else
-        {
-            monster.MoveTowards(monster.player.transform.position);
-        }
-    }
+	public override void UpdateState()
+	{
+		if (!(monster as Turret).isAttacking && monster.IsPlayerInRange(monster.monsterBaseStat.attackRange))
+		{
+			monster.TransitionToState(monster.attackState);
+		}
+		else
+		{
+			monster.MoveTowards(monster.player.transform.position);
+		}
+	}
 
-    public override void ExitState() { }
+	public override void ExitState() { }
 }
 
 public class TurretAttackState : MonsterState
 {
-    public TurretAttackState(Monster monster) : base(monster) { }
+	public TurretAttackState(Monster monster) : base(monster) { }
 
-    public override void EnterState()
-    {
-        monster.Attack();
-    }
+	public override void EnterState()
+	{
+		monster.Attack();
+	}
 
-    public override void UpdateState()
-    {
-        if (!(monster as Turret).isAttacking && !monster.IsPlayerInRange(monster.monsterBaseStat.attackRange))
-        {
-            monster.TransitionToState(monster.idleState);
-        }
-    }
+	public override void UpdateState()
+	{
+		if (!(monster as Turret).isAttacking && !monster.IsPlayerInRange(monster.monsterBaseStat.attackRange))
+		{
+			monster.TransitionToState(monster.idleState);
+		}
+	}
 
-    public override void ExitState() { }
+	public override void ExitState() { }
 }
 
 public class TurretCooldownState : MonsterState
 {
-    private float cooldownTimer;
+	private float cooldownTimer;
 
-    public TurretCooldownState(Monster monster) : base(monster) { }
+	public TurretCooldownState(Monster monster) : base(monster) { }
 
-    public override void EnterState()
-    {
-        cooldownTimer = (monster.monsterBaseStat as TurretMonsterData).attackCooldown;
-        monster.isInCooldown = true;
-    }
+	public override void EnterState()
+	{
+		cooldownTimer = (monster.monsterBaseStat as TurretMonsterData).attackCooldown;
+		monster.isInCooldown = true;
+	}
 
-    public override void UpdateState()
-    {
-        cooldownTimer -= Time.deltaTime;
-        if (cooldownTimer <= 0)
-        {
-            monster.isInCooldown = false;
-            if (monster.IsPlayerInRange(monster.monsterBaseStat.detectionRange))
-            {
-                monster.TransitionToState(monster.chaseState);
-            }
-            else
-            {
-                (monster as Turret)?.PlayAnimation((monster as Turret).idleAnimation, true);
-                monster.TransitionToState(monster.idleState);
-            }
-        }
-    }
+	public override void UpdateState()
+	{
+		cooldownTimer -= Time.deltaTime;
+		if (cooldownTimer <= 0)
+		{
+			monster.isInCooldown = false;
+			if (monster.IsPlayerInRange(monster.monsterBaseStat.detectionRange))
+			{
+				monster.TransitionToState(monster.chaseState);
+			}
+			else
+			{
+				(monster as Turret)?.PlayAnimation((monster as Turret).idleAnimation, true);
+				monster.TransitionToState(monster.idleState);
+			}
+		}
+	}
 
-    public override void ExitState() { }
+	public override void ExitState() { }
 }
