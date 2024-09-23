21dfc91005416b7ad0c6b57eef02d56e0d7ade66
DESKTOP-81380DR\Hyeon2-400 <choihg1014@gmail.com>
2024-09-23 15:45:01 +0900

    [#main] CCRC 백업

--- Assets/3-Scripts/Monster/Sloth/Turret.cs
+++ Assets/3-Scripts/Monster/Sloth/Turret.cs
@@ -296,7 +296,7 @@ public class TurretCooldownState : MonsterState
 
     public override void UpdateState()
     {
-        cooldownTimer -= Time.unscaledDeltaTime;
+        cooldownTimer -= Time.deltaTime;
         if (cooldownTimer <= 0)
         {
             monster.isInCooldown = false;
